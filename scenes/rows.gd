89dd3acf3a5b8a91285ae982c6f2e48e61323e49
Logan Croley <logan.croley.18@gmail.com>
2024-10-06 13:47:07 -0400

    removed unnessacary file

--- scenes/rows.gd
+++
@@ -1,8 +0,0 @@
-extends VBoxContainer
-class_name Rows
-
-@onready var row_1: HBoxContainer = $Row1
-@onready var row_2: HBoxContainer = $Row2
-@onready var row_3: HBoxContainer = $Row3
-@onready var row_4: HBoxContainer = $Row4
-@onready var row_5: HBoxContainer = $Row5
