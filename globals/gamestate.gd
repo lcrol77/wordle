bb543970d3cd821ec414cc2e7e38605caba2da5b
Logan Croley <logan.croley.18@gmail.com>
2024-10-08 14:41:54 -0400

    can host and connect to server. Cannot start game yet though

--- globals/gamestate.gd
+++ globals/gamestate.gd
@@ -1,7 +1,10 @@
 extends Node
 const DEFAULT_PORT = 10567
-const MAX_PEERS = 4
+const MAX_PEERS = 5
 var peer: ENetMultiplayerPeer
+
+## Our local player's name.
+var player_name := "The Warrior"
 
 # Names for remote players in id:name format.
 var players := {}
@@ -13,3 +16,82 @@ signal connection_failed()
 signal connection_succeeded()
 signal game_ended()
 signal game_error(what: int)
+
+func _ready() -> void:
+	multiplayer.peer_connected.connect(_player_connected)
+	multiplayer.peer_disconnected.connect(_player_disconnected)
+	multiplayer.connected_to_server.connect(_connected_ok)
+	multiplayer.connection_failed.connect(_connected_fail)
+	multiplayer.server_disconnected.connect(_server_disconnected)
+
+func get_player_list() -> Array:
+	return players.values()
+	
+# Callback from SceneTree.
+func _player_connected(id: int) -> void:
+	# Registration of a client beings here, tell the connected player that we are here.
+	register_player.rpc_id(id, player_name)
+
+
+#region Mutliplayer Callbacks
+func _player_disconnected(id: int) -> void:
+	if has_node("/root/World"):
+		# Game is in progress.
+		if multiplayer.is_server():
+			game_error.emit("Player " + players[id] + " disconnected")
+			end_game()
+	else:
+		# Game is not in progress.
+		# Unregister this player.
+		unregister_player(id)
+
+
+# Callback from SceneTree, only for clients (not server).
+func _connected_ok() -> void:
+	# We just connected to a server
+	connection_succeeded.emit()
+
+
+# Callback from SceneTree, only for clients (not server).
+func _server_disconnected() -> void:
+	game_error.emit("Server disconnected")
+	end_game()
+
+
+# Callback from SceneTree, only for clients (not server).
+func _connected_fail() -> void:
+	multiplayer.set_network_peer(null) # Remove peer
+	connection_failed.emit()
+#endregion
+#region Lobby Management Functions
+@rpc("any_peer")
+func register_player(new_player_name: String) -> void:
+	var id := multiplayer.get_remote_sender_id()
+	players[id] = new_player_name
+	player_list_changed.emit()
+
+
+func unregister_player(id: int) -> void:
+	players.erase(id)
+	player_list_changed.emit()
+
+func host_game(new_player_name: String) -> void:
+	player_name = new_player_name
+	peer = ENetMultiplayerPeer.new()
+	peer.create_server(DEFAULT_PORT, MAX_PEERS)
+	multiplayer.set_multiplayer_peer(peer)
+
+func join_game(ip: String, new_player_name: String) -> void:
+	player_name = new_player_name
+	peer = ENetMultiplayerPeer.new()
+	peer.create_client(ip, DEFAULT_PORT)
+	multiplayer.set_multiplayer_peer(peer)
+
+func end_game() -> void:
+	if has_node("/root/Main"):
+		# If the game is in progress, end it.
+		get_node("/root/Main").queue_free()
+
+	game_ended.emit()
+	players.clear()
+#endregion
